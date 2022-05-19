21d44cf5410b2faab9806ccddb5b6adbd5ab2f19
luc4srocha <lucas.rocha28@outlook.com>
2022-05-18 23:05:03 -0300

    Create exercicio1.py

---
+++ exercicio1.py
@@ -0,0 +1,11 @@
+"""Exercício 1: Faça um programa que peça ao usuário um número e imprima todos
+ os números de um até o número que o usuário informar."""
+
+numero = int(input('Digite um numero até 10: '))
+
+n = 1
+
+while n <= numero:
+    print (n)
+    n = n + 1
+
